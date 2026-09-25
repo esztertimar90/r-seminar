b8805bcf867854aa5bf32553fdaaf553731446dc
Eszter Timar <eszter.timar90@gmail.com>
2026-09-25 14:33:19 +0200

    added joins script and football data

--- code/preliminary/lecture03.R
+++ code/preliminary/lecture03.R
@@ -1,13 +1,14 @@
 # ------------------------------------------------------------------------------
-# Import data, wide and long format
-# based on
-#   - Ágoston Reguly, Gábor Békés: Introduction to Data Analysis with R - lecture materials
-#
+# Joining and merging data #
+# Session 3 - 28 September 
+# written by Eszter Timar 
+# ------------------------------------------------------------------------------
 ## cleaning the environment ----------------------------------------------------
 rm(list = ls())
 
 ## packages/libraries ----------------------------------------------------------
 # load packages
+install.packages("tidyverse")
 library(tidyverse)
 
 ## 1) case study: football managers --------------------------------------------
@@ -19,130 +20,125 @@ rm(list = ls())
 
 # working directory, paths
 getwd() # your working directory: this is where you are "standing" in your folder structure
-setwd('/Users/martinneubrandt/Library/CloudStorage/Dropbox/GitHub/corvinus_econometrics') # change the working directory
+setwd('/Users/esztertimar/Teaching/Econometrics R Seminar 2026-27') # change the working directory
+
+# We will use a case study on how to identify successful football managers based on their experience (number of games) and their performance (points). 
+# The data is available in two separate files: games.csv and points.csv. 
+# We will import, clean, transform, and merge these datasets to analyze the relationship between experience and performance.
 
 # import data
-# set data input paths
-games_path <- 'data/football_managers/'
-points_url <- 'https://raw.githubusercontent.com/gabors-data-analysis/da-coding-rstats/main/lecture03-tibbles/data/'
-
-games <- read_csv(paste0(games_path, 'games.csv'))
-points <- read_csv(paste0(points_url, 'points.csv'))
-
-# transform to tidy data: long format
-# first we need a new variable, which contains the team names
-team_names = games |> 
-  select(-manager_id, -manager_name) |> # exclude the first two columns (manager id and name)
-  names() # get the names of the remaining columns
-
-# use team names for transformation to long format
-tidy_games <- pivot_longer(data = games, 
-                           cols = team_names,
-                           names_to = 'team',
-                           values_to = 'manager_games')
+games <- read_csv('data/games.csv')
+points <- read_csv('data/points.csv')
 
 # clean missing values
-tidy_games <- tidy_games |> 
-  filter(!is.na(manager_games))
+games <- filter(games, !is.na(manager_games))
+points <- filter(points, !is.na(manager_points))
 
-# transform to wide format (from tidy to non-tidy format)
-wide_games <- pivot_wider(data = tidy_games, 
-                          names_from = team, 
-                          values_from = manager_games)
+view(games)
+view(points)
 
-# merge the two data sets
+# How do we define manager success?
+# Let's take the average points per game. 
+# We need to create one data set that contains the number of games and points for each manager.
+
+# merge the two data sets: called "join" in R language 
+# there are different types of joins: left join, right join, full join, inner join
+
 # left join:
-# - keeps all observations from the left data set (x) (now: games)
+# - keeps all observations from the left data set (x) (now: games) (1:m in STATA)
 # - adds observations from the right data set (y) (points) where the keys match
 # - if there is no match, the new variables are filled with NA
-games_points_left <- left_join(x = tidy_games,
+games_points_left <- left_join(x = games,
                                y = points,
                                by = c('team', 'manager_id', 'manager_name'))
+view(games_points_left)
+
 # right join:
-# - keeps all observations from the right data set (y) (now: points)
+# - keeps all observations from the right data set (y) (now: points) (m:1 in STATA)
 # - adds observations from the left data set (x) (games) where the keys match
 # - if there is no match, the new variables are filled with NA
-games_points_right <- right_join(x = tidy_games,
+games_points_right <- right_join(x = games,
                                  y = points,
                                  by = c('team', 'manager_id', 'manager_name'))
+view(games_points_right)
 
-# full join:
-# - keeps all observations from both data sets
+all.equal(games_points_left, games_points_right)  # they are not equal because the two data sets have different number of observations
+
+# full join: 
+# - keeps all observations from both data sets (m:m in STATA)
 # - adds observations where the keys match
 # - if there is no match, the new variables are filled with NA
-games_points_full <- full_join(x = tidy_games,
+games_points_full <- full_join(x = games,
                                y = points,
                                by = c('team', 'manager_id', 'manager_name'))
 
 # inner join:
-# - keeps only the observations where the keys match in both data sets
-games_points_inner <- inner_join(tidy_games,
+# - keeps only the observations where the keys match in both data sets (1:1 in STATA)
+games_points_inner <- inner_join(games,
                                  points,
                                  by = c('team', 'manager_id', 'manager_name'))
 
+# anti-join keeps observations with no match - we rarely need this
+games_points_anti <- anti_join(x = games,
+                                y = points,
+                                by = c('team', 'manager_id', 'manager_name'))
+print(games_points_anti$manager_name) # managers with no points
+
 # Importance of the key-variables or identifiers:
+# What happens if we don't have a unique ID? Let's try without naming managers:
+
 # No unique identifier: create multiple new variables and observations
-no_unique <- left_join(x = tidy_games,
+no_unique <- left_join(x = games,
                        y = points,
                        by = c('team'))
 
-# Unique identifier, but unmatched variable with the same name: create (separate) new variables
-unmatched_variable <- left_join(x = tidy_games,
-                                y = points,
-                                by = c('team','manager_id'))
+# We proceed with the inner-joined data because it does not have missing values: 
+view(games_points_inner) 
 
-# aggregate to manager level
-manager_level <- games_points_inner |> 
-  group_by(manager_name, manager_id) |> 
-  summarise(num_teams = n_distinct(team),
-            total_games = sum(manager_games),
-            total_points = sum(manager_points),
-            avg_points_per_game = round(total_points/total_games, 2)) |> 
-  arrange(manager_id)
-
-# plot total points and number of games by managers
-manager_level |> 
-  ggplot(aes(x = total_games,
-             y = total_points)) +
-  geom_point() +
-  geom_smooth(method = 'lm')
-
-# plot average points and number of games by managers
-manager_level |> 
-  ggplot(aes(x = total_games,
-             y = avg_points_per_game)) +
-  geom_point() +
-  geom_smooth(method = 'lm')
-
-# transform number of teams to a factor variable, categories: 1, 2, 3+
-manager_level <- manager_level |> 
-  mutate(num_teams = factor(ifelse(num_teams == 1, '1',
-                                   ifelse(num_teams == 2, '2', '3+')),
-                            levels = c('1', '2', '3+')))
-
-# scatter plot about the average points and number of games
-# colour by number of teams managed, add labels, and change some elements in the theme
-manager_level |> 
-  ggplot(aes(x = total_games,
-             y = avg_points_per_game)) +
-  geom_point(aes(color = num_teams),
-             size = 5,
-             alpha = 0.7) +
-  geom_smooth(method = 'lm', 
-              se = FALSE, 
-              color = 'black',
-              linetype = 'dashed',
-              size = 1) +
-  labs(title = 'Average points per game vs. total games of football managers',
-       x = 'Total games',
-       y = 'Average points per game',
-       color = 'Number of teams managed') +
+# Plot points and games per manager:
+ggplot(games_points_inner, aes(x = manager_games, y = manager_points)) +
+  geom_point(stat = 'identity', fill = 'steelblue') +
   theme_minimal() +
-  theme(text = element_text(size = 18),
-        legend.position = 'top')
+  labs(title = 'Points vs Games per Manager',
+       x = 'Number of Games',
+       y = 'Number of Points')
 
-# save the figure
-ggsave(filename = 'manager_points.png',
-       path = 'output/figure/',
-       width = 10,
-       height = 6)
+# We need to calculate average score by manager (some managers switched teams)
+manager_avg <- games_points_inner |>
+  group_by(manager_id) |>
+  summarise(avg_points = sum(manager_points) / sum(manager_games)) 
+
+head(manager_avg)
+
+# Let's add the manager averages as a new column to our tibble:
+football <- left_join(x = games_points_inner,
+                                y = manager_avg,
+                                by = c('manager_id'))
+
+# We can sort the data based on the average points:
+football
+football <- football |>
+          arrange(-avg_points)
+summary(football$avg_points)
+
+view(football)
+
+ggplot(manager_avg, aes(x = reorder(manager_id, avg_points), y = avg_points)) +
+  geom_col(fill = 'steelblue') +
+  theme_minimal() +
+  labs(title = 'Average Points per Game by Manager',
+       x = 'Manager ID',
+       y = 'Average Points per Game') +
+  theme(axis.text.x = element_blank())
+
+ggsave('graphs/manager_scores.png')
+
+# So who is the winner? 'Slice' and 'Pull' functions helps us find him:
+football |>
+  slice_max(avg_points, n = 1, with_ties = TRUE) |>
+  pull(manager_name, team)
+
+# And the worst performers:
+football |>
+  slice_min(avg_points, n = 1, with_ties = TRUE) |>
+  pull(manager_name, team)
